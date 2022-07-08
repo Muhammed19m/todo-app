42a6781efdc4134b10e6c9ab98d0515d90986d18
Muhammed19m <71428103+Muhammed19m@users.noreply.github.com>
2022-07-08 11:11:18 +0300

    data changes

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,5 @@
-pub const HOST: &'static str = "http://localhost:8000";
-pub const EMAIL: &str = "axumchat@gmail.com";
-pub const PASSWORD: &str = "ngwbxowcnfbotdbq";
+pub const HOST: &'static str = "**************";
+pub const EMAIL: &str = "**************";
+pub const PASSWORD: &str = "**************";
 
-pub const HOST_DB: &str = "mysql://root:xdrtreszxcft@localhost/todo_app";
+pub const HOST_DB: &str = "**************";
