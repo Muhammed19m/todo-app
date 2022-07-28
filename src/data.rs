dcf0ae2b039cdae797aeb8d4bed268edc814db4d
Muhammed19m <71428103+Muhammed19m@users.noreply.github.com>
2022-07-28 22:56:35 +0300

    delete some file

--- src/data.rs
+++
@@ -1,5 +0,0 @@
-pub const HOST: &'static str = "**************";
-pub const EMAIL: &str = "**************";
-pub const PASSWORD: &str = "**************";
-
-pub const HOST_DB: &str = "**************";
